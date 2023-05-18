1112a0161ef4f3d52655c879063141a22af75b7b
Nirob Barman <niroborin111@gmail.com>
2023-05-18 18:37:31 +0600

    react router and firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCC4jsS_v_iTHZQ1oaOz0xV7X3PgZPemlY",
+    authDomain: "toy-marketplace-5c0d6.firebaseapp.com",
+    projectId: "toy-marketplace-5c0d6",
+    storageBucket: "toy-marketplace-5c0d6.appspot.com",
+    messagingSenderId: "743524393301",
+    appId: "1:743524393301:web:5f801f7fe47e8853608fdc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
