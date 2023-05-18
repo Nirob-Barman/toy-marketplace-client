14a26b043e1e3ca2cc1e7b3df2ae17f182c8b75b
Nirob Barman <niroborin111@gmail.com>
2023-05-18 22:17:44 +0600

    Navigation Bar Processing

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,37 +1,117 @@
-import React from 'react';
-import { Button, Container, Form, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
+import Toast from 'react-bootstrap/Toast';
+import { FaGoogle } from 'react-icons/fa';
 
 const LoginPage = () => {
-    const handleLogin = () => {
-        // Handle login logic here
-    };
+
+    const [error, setError] = useState('');
+
+    const [show, setShow] = useState(false);
+
+    const { signIn, signInWithGoogle } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    // console.log('login page location', location)
+    const from = location.state?.from?.pathname || '/'
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        setError('');
+        if (password.length < 6) {
+            setError('Password must be 6 characters long');
+            return
+        }
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                setError('');
+
+                navigate(from, { replace: true })
+
+            })
+            .catch(error => {
+                // console.log(error.message);
+                setError(error.message);
+            })
+    }
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then((result) => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+
 
     return (
-        <Container>
-            <Row className="justify-content-center">
-                <Col xs={12} md={6}>
-                    <div className="login-form">
-                        <h2>Login</h2>
-                        <Form>
-                            <Form.Group controlId="email">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter your email" />
-                            </Form.Group>
+        <Container className=''>
+            <Row className='justify-content-center'>
+                <Col lg={6}>
+                    <h3 className='text-center mt-2'>Please Login</h3>
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Enter your Email address</Form.Label>
+                            <Form.Control type="email" name='email' placeholder="Enter email" required />
+                        </Form.Group>
 
-                            <Form.Group controlId="password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Enter your password" />
-                            </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Enter your Password</Form.Label>
+                            <Form.Control type="password" name='password' placeholder="Password" required />
+                        </Form.Group>
 
-                            <Button variant="primary" onClick={handleLogin}>Login</Button>
-                            <Button variant="secondary" className="google-login">Google Sign-in</Button>
-                        </Form>
+                        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+                        {/* <p className='text-success'>{error}</p> */}
+                        {
+                            error && <Toast
+                                onClose={() => setShow(false)} show={show} delay={2000} autohide
+                                style={{
+                                    position: 'absolute',
+                                    top: '32%',
+                                    left: '57%',
+                                    transform: 'translate(-50%, -50%)',
+                                }}
+                            >
+                                <Toast.Header />
 
-                        <div className="register-link">
-                            Don't have an account? <Link to="/register">Register here</Link>
+                                <Toast.Body className='text-danger'>{error}</Toast.Body>
+                            </Toast>
+                        }
+                        <div className='text-center'>
+                            <Button onClick={() => setShow(true)} variant="primary" type="submit" >
+                                Login
+                            </Button>
+                            <hr />
+                            <Button className='mt-2' onClick={handleGoogleSignIn} variant="danger">
+                                Login with <FaGoogle />
+                            </Button>
+                            <hr />
                         </div>
-                    </div>
+                        <div className='text-center mt-2'>
+                            <Form.Text className="text-secondary">
+                                Don't Have an Account? <Link to="/register">Register</Link>
+                            </Form.Text>
+                        </div>
+
+                    </Form>
                 </Col>
             </Row>
         </Container>
--- src/Pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/Pages/Shared/NavigationBar/NavigationBar.jsx
@@ -1,63 +1,163 @@
-import React, { useState } from 'react';
-import { Navbar, Nav, NavDropdown, Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Navbar, Nav, NavDropdown, Button, Container, Image } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 // import logo from './path/to/logo.png';
 
 const NavigationBar = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [username, setUsername] = useState('');
 
+    const location = useLocation();
+
+    const { user, logOut } = useContext(AuthContext);
+    // console.log(user);
+    // console.log(user.displayName);
+    // console.log(user.photoURL);
+    // console.log(user.email);
+    const handleLogOut = () => {
+        logOut()
+            .then()
+            .catch(error => console.log(error));
+    }
+
     const handleLogin = () => {
         // Handle login logic here
-        setIsLoggedIn(true);
-        setUsername('John Doe'); // Replace with the actual username
+        // setIsLoggedIn(true);
+        // setUsername('John Doe'); 
     };
 
     const handleLogout = () => {
         // Handle logout logic here
-        setIsLoggedIn(false);
-        setUsername('');
+        // setIsLoggedIn(false);
+        // setUsername('');
     };
 
-    const handleUsernameHover = () => {
-        // Handle username hover behavior here
-        // For example, show a tooltip or additional information
-    };
+    // const handleUsernameHover = () => {
+    //     // Handle username hover behavior here
+    //     // For example, show a tooltip or additional information
+    // };
 
 
     return (
-        <Container>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">
-                    <img
-                        // src={logo}
-                        alt="Website Logo" className="navbar-logo" />
-                    Website Name
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbar-nav" />
-                <Navbar.Collapse id="navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/all-toys">All Toys</Nav.Link>
-                        <Nav.Link href="/blogs">Blogs</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        {isLoggedIn ? (
-                            <>
-                                <Nav.Link href="/my-toys">My Toys</Nav.Link>
-                                <Nav.Link href="/add-toy">Add A Toy</Nav.Link>
-                                <Nav.Link className="username" onMouseEnter={() => setUsername('')}>
-                                    {username}
-                                </Nav.Link>
-                                <Button variant="outline-primary" onClick={handleLogout}>Logout</Button>
-                            </>
-                        ) : (
+        <div>
+
+            <Container>
+                <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Container>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav align-items-center">
+                            <Nav>
+                                <Link
+                                    title='Delicious'
+                                    to="/">
+                                    <Image
+                                        className=''
+                                        // src="https://w7.pngwing.com/pngs/791/461/png-transparent-seed-bank-seed-company-hemp-benih-others-text-wedding-logo.png"
+                                        style={{ width: "100%", height: "40px", objectFit: "cover" }}
+                                        alt="Delicious" />
+                                </Link>
+                            </Nav>
+                            <Nav className="mx-auto gap-5">
+                                <Link to="/" className={`text-decoration-none ${location.pathname === '/' ? 'fs-6 text-uppercase text-danger text-decoration-none' : ''}`}
+                                // className={location.pathname === '/' ? 'text-danger text-decoration-none' : ''}
+                                >Home</Link>
+                                <Link to="/blog"
+                                    className={`text-decoration-none ${location.pathname === '/blog' ? 'fs-6 text-uppercase text-danger text-decoration-none' : ''}`}
+                                // className={location.pathname === '/blog' ? 'text-danger text-decoration-none' : ''}
+                                >Blog</Link>
+                                <Link to="/about"
+                                    className={`text-decoration-none ${location.pathname === '/about' ? 'fs-6 text-uppercase text-danger text-decoration-none' : ''}`}
+                                // className={location.pathname === '/about' ? 'text-danger text-decoration-none' : ''}
+                                >About</Link>
+                            </Nav>
+
+                            <Nav className="mx-auto">
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/all-toys">All Toys</Nav.Link>
+                                <Nav.Link href="/blogs">Blogs</Nav.Link>
+                            </Nav>
+
+                            <Nav>
+                                {user && (
+                                    <>
+                                        <Nav.Link href="/my-toys">My Toys</Nav.Link>
+                                        <Nav.Link href="/add-toy">Add A Toy</Nav.Link>
+                                        <Nav.Link className="username" onMouseEnter={() => setUsername('')}>
+                                            {username}
+                                        </Nav.Link>
+                                    </>
+                                ) }
+                            </Nav>
+
+                            <Nav className='gap-2'>
+                                {
+                                    user &&
+                                    <Image
+                                        title={user?.displayName}
+                                        src={user?.photoURL}
+                                        roundedCircle
+                                        style={{ width: "50px", height: "50px" }}
+                                        alt="profile" fluid />
+
+                                    // <Card.Img
+                                    // roundedCircle 
+                                    // title={user?.displayName}
+                                    // style={{ height: '2.5rem', }}
+                                    //  src={user.photoURL} />
+
+                                    // // :
+                                    // <FaUser
+                                    //     title={user?.displayName}
+                                    //     style={{ fontSize: '2rem' }}></FaUser>
+                                }
+
+                                {user ?
+                                    <Button onClick={handleLogOut} variant="secondary">Logout</Button> :
+                                    <Link to="/login">
+                                        <Button variant="secondary">Login</Button>
+                                    </Link>
+                                }
+
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </Container>
+
+            {/* <Container>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="/">
+                        <img
+                            // src={logo}
+                            alt="Website Logo" className="navbar-logo" />
+                        Website Name
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbar-nav" />
+                    <Navbar.Collapse id="navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/all-toys">All Toys</Nav.Link>
+                            <Nav.Link href="/blogs">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {user ? (
+                                <>
+                                    <Nav.Link href="/my-toys">My Toys</Nav.Link>
+                                    <Nav.Link href="/add-toy">Add A Toy</Nav.Link>
+                                    <Nav.Link className="username" onMouseEnter={() => setUsername('')}>
+                                        {username}
+                                    </Nav.Link>
+                                    <Button variant="outline-primary" onClick={handleLogout}>Logout</Button>
+                                </>
+                            ) : (
                                 <Link to="/login"><Button variant="primary" onClick={handleLogin}>Login</Button></Link>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Container>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container> */}
+        </div>
     );
 };
 
