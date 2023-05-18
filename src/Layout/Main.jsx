d7f64418497313ac765e6b842eead61ca46ae470
Nirob Barman <niroborin111@gmail.com>
2023-05-18 18:57:12 +0600

    sdk setup and configuration and react router added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Pages/Shared/Header/Header';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
