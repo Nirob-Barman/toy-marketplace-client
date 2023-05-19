aa64d2c47142a548a07217e42646a824046291dc
Nirob Barman <niroborin111@gmail.com>
2023-05-19 09:38:29 +0600

    Banner and Gallery Section sample page added

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { Container, Row, Col, Badge } from 'react-bootstrap'
 
 const Banner = () => {
+
+    const marqueeRef = useRef(null);
+
+    useEffect(() => {
+        // Start the marquee animation when the component mounts
+        marqueeRef.current.start();
+        return () => {
+            // Stop the marquee animation when the component unmounts
+            marqueeRef.current.stop();
+        };
+    }, []);
+
     return (
-        <div>
-            Banner content
+        <div className="bg-dark py-4">
+            <Container>
+                <Row>
+                    <Col>
+                        <h1 className="text-white">Welcome to ToyVerse!</h1>
+                        <p className="text-white">
+                            Discover a world of amazing toys for kids of all ages.
+                        </p>
+                        <marquee ref={marqueeRef} direction="left" scrollamount="5">
+                            <p className="text-white">
+                                <Badge bg="danger">Special Offer:</Badge> Get 20% off on all toys this week! Limited time only.
+                            </p>
+                        </marquee>
+                        <p className="text-white">
+                            Shop now and bring joy to your little ones!
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Gallery = () => {
+    const images = [
+        { src: 'https://img.freepik.com/premium-photo/toy-car-isolated_130040-1833.jpg?w=740', alt: 'Image 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+        { src: 'https://img.freepik.com/premium-photo/toy-car-isolated_130040-1833.jpg?w=740', alt: 'Image 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+        { src: 'https://img.freepik.com/premium-photo/toy-car-isolated_130040-1833.jpg?w=740', alt: 'Image 3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+        { src: 'https://img.freepik.com/premium-photo/toy-car-isolated_130040-1833.jpg?w=740', alt: 'Image 4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+        
+        // Add more images here
+    ];
+
     return (
-        <div>
-            Gallery content
+        <div className="py-5">
+
+            <Container>
+                <h2 className="text-center mb-4">Gallery</h2>
+                <Row>
+                    {images.map((image, index) => (
+                        <Col xs={12} sm={6} md={4} lg={3} key={index} className="mb-4">
+                            <Card>
+                                <Card.Img
+                                    src={image.src}
+                                    alt={image.alt}
+                                    className="img-fluid gallery-image"
+                                />
+                                <Card.Body>
+                                    <Card.Title>{image.alt}</Card.Title>
+                                    <Card.Text>{image.description}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+            
         </div>
     );
 };
