c658193ab2d13e5fa45424362371798b37562fc8
Nirob Barman <niroborin111@gmail.com>
2023-05-19 13:10:31 +0600

    aos implementation

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -7,9 +7,18 @@ import SocialSharing from '../../SocialSharing/SocialSharing';
 import Customization from '../../Customization/Customization';
 import ReviewsAndRatings from '../../ReviewsAndRatings/ReviewsAndRatings';
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
+
 const Header = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
-        <Container className='my-5'>
+        <Container data-aos="fade-up" className='my-5'>
             <Banner />
             <Gallery />
             <ReactTabs />
