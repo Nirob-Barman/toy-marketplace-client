21504a4d53f69f7e58cb5bd98b58b840e82558b2
Nirob Barman <niroborin111@gmail.com>
2023-05-21 07:25:16 +0600

    Banner Updated

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,16 +1,40 @@
 
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 import { Container, Button, Badge } from 'react-bootstrap';
 
 const Banner = () => {
+
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const containerElement = containerRef.current;
+        const computedStyle = getComputedStyle(containerElement);
+        const backgroundColor = computedStyle.backgroundColor;
+        const colorThreshold = 128; // Threshold for determining dark or light background
+
+        // Get the RGB values of the background color
+        const rgbValues = backgroundColor.match(/\d+/g);
+        const r = Number(rgbValues[0]);
+        const g = Number(rgbValues[1]);
+        const b = Number(rgbValues[2]);
+
+        // Calculate the brightness of the background color
+        const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+        // Set the text color based on the brightness of the background
+        const textColor = brightness > colorThreshold ? "black" : "white";
+        containerElement.style.color = textColor;
+    }, []);
+
     return (
         <div className="banner">
-            <Container>
+            <Container ref={containerRef} style={{ border: "1px solid black", background: "linear-gradient(to bottom, #9b59b6, #8e44ad)", padding: "20px", margin: "20px" }}>
+                {/* The rest of your code */}
                 <div className="banner-content">
                     <h1>Welcome to Toyverse!</h1>
                     <p>
                         <marquee behavior="scroll" direction="left">
-                            <span className="text-primary">
+                            <span className="text-white">
                                 <Badge bg="danger">Special Offer:</Badge> Get 20% off on all toys this week! Limited time only.
                             </span>
                         </marquee>
